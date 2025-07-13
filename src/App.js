14a8f2f37b6cb69f14338bdc342d7480e7b1ec98
thisisrow <prathameshmisra2020@gmail.com>
2025-07-13 17:30:37 +0530

    chore: initialize project with package.json and essential dependencies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
-    React.createElement("p", {},props.description),
+    React.createElement("p", {}, props.description),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Pixel Perfect Pizzas"),
-    React.createElement(Pizza,{name:"pazz1",description:" kcbsabci dasbd dqwoidw"}),
-    React.createElement(Pizza,{name:"pazz1",description:" kcbsabci dasbd dqwoidw"}),
-    React.createElement(Pizza,{name:"pazz1",description:" kcbsabci dasbd dqwoidw"}),
+    React.createElement(Pizza, {
+      name: "pazz1",
+      description: " kcbsabci dasbd dqwoidw",
+    }),
+    React.createElement(Pizza, {
+      name: "pazz1",
+      description: " kcbsabci dasbd dqwoidw",
+    }),
+    React.createElement(Pizza, {
+      name: "pazz1",
+      description: " kcbsabci dasbd dqwoidw",
+    }),
   ]);
 };
 
